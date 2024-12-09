6b7beab290fd31e6a8f97dd13d9300e96dda624b
Temidire <temidireowoeye@gmail.com>
2024-12-09 10:13:55 +0100

    feat: built ball canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
 
-const Ball = () => {
+import CanvasLoader from '../Loader'
+
+import PropTypes from "prop-types";
+
+
+const Ball = (props) => {
+
+  const [ decal ] = useTexture([props.imageUrl])
+
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1,1]} />
+        <meshStandardMaterial color={'#fff8eb'} polygonOffset polygonOffsetFactor={-5} flatShading />
+        <Decal position={[0,0,1]} rotation={[ 2 * Math.PI, 0, 6.25]} flatShading map={decal}/>
+      </mesh>
+    </Float>
   )
 }
 
-export default Ball
+Ball.propTypes = PropTypes.any.isRequired
+
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+    frameloop='demand'
+    // dpr={[1, 2]}
+    gl={{ preserveDrawingBuffer: true }}
+  >
+    <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls autoRotate enableZoom={false}/>
+      <Ball imageUrl={icon} />
+    </Suspense>
+
+    <Preload all />
+  </Canvas>
+  )
+}
+
+BallCanvas.propTypes = {
+  icon: PropTypes.any.isRequired
+}
+
+export default BallCanvas
