653b229171b346f72c0e628d849592dbff562888
Temidire <temidireowoeye@gmail.com>
2024-12-10 18:39:15 +0100

    fix: hide scroll-to-about on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,7 +67,7 @@ const Hero = () => {
 
       {/* <ComputersCanvas /> */}
     </section>
-    <div className=' w-full flex justify-center items-center'>
+    <div className=' w-full justify-center hidden md:flex items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <m.div
